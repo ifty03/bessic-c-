619c40eeaa16ab041c634bbc6bf9a9f01acd9864
ifty03 <ashikulislamb84@gmail.com>
2023-11-15 18:19:33 -1200

    comparison problem solved

---
+++ V_Comparison.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    char s;
+    int b;
+
+    cin >> a >> s >> b;
+
+    if (s == '=')
+    {
+        if (a == b)
+        {
+            cout << "Right" << endl;
+        }
+        else
+        {
+            cout << "Wrong" << endl;
+        }
+    }
+    else if (s == '>')
+    {
+        if (a > b)
+        {
+            cout << "Right" << endl;
+        }
+        else
+        {
+            cout << "Wrong" << endl;
+        }
+    }
+    else
+    {
+        if (a < b)
+        {
+            cout << "Right" << endl;
+        }
+        else
+        {
+            cout << "Wrong" << endl;
+        }
+    }
+    return 0;
+}
