f981de4d3953650c1457f0fd444ce6dba04e7ca1
ifty03 <ashikulislamb84@gmail.com>
2023-11-23 20:29:47 +0600

    create class

---
+++ min.arrayOfObject.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student{
+    public:
+    string name;
+    int marks;
+    int roll;
+
+};
+int main(){
+int n;
+cin>>n;
+
+Student ar[n];
+
+for(int i=0;i<n;i++){
+cin>>ar[i].name>>ar[i].roll>>ar[i].marks;
+}
+for(int i=0;i<n;i++){
+cout<<ar[i].name<<" "<<ar[i].roll<<" "<<ar[i].marks<<endl;
+}
+
+    return 0;
+}
