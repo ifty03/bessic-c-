f1899ee6784ade15e19f2ec199329de4dfa5b2e9
ifty03 <ashikulislamb84@gmail.com>
2023-11-25 13:05:22 +0600

    done

--- Sort_It.cpp
+++ Sort_It.cpp
@@ -1,27 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Student
+{
+public:
+    string name;
+    int cls;
+    char section;
+    int roll;
+    int mMarks;
+    int eMarks;
+};
 
+bool st(Student a, Student b)
+{
+    int aTotal = a.eMarks + a.mMarks;
+    int bTotal = b.eMarks + b.mMarks;
+
+    if (aTotal == bTotal)
+    {
+        return a.roll < b.roll;
+    }
+    else
+    {
+
+        return aTotal > bTotal;
+    }
+}
 int main()
 {
     int n;
     cin >> n;
-    int ar[n];
+
+    Student ar[n];
 
     for (int i = 0; i < n; i++)
     {
-        cin >> ar[i];
+        cin >> ar[i].name >> ar[i].cls >> ar[i].section >> ar[i].roll >> ar[i].mMarks >> ar[i].eMarks;
     }
-    sort(ar, ar + n);
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << ar[i] << " ";
-    }
-    cout << endl;
-    sort(ar, ar + n, greater<int>());
-    for (int i = 0; i < n; i++)
-    {
-        cout << ar[i] << " ";
+    sort(ar, ar + n, st);
+
+    for(int i=0;i<n;i++){
+    cout<<ar[i].name<<" "<<ar[i].cls<<" "<<ar[i].section<<" "<<ar[i].roll<<" "<<ar[i].mMarks<<" "<<ar[i].eMarks<<endl;
     }
 
     return 0;
